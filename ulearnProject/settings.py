27c491f24f5f0da976d285f52f7b5b5aadf16f97
Kexogg <kexogg@gmail.com>
2024-01-16 21:09:48 +0500

    update settings.py

--- ulearnProject/settings.py
+++ ulearnProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=x*21sfl9yjs%$9-rhso$m3cl!5rmv*@2zczy+_959qi213$dj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = ['ulearn.kexogg.ru']
 
 
 # Application definition
