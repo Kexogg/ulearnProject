cf919c4fc05e24f30af53d6c2b6e2e17b60363ba
Kexogg <kexogg@gmail.com>
2024-01-18 18:45:22 +0500

    models

--- ulearnProject/admin.py
+++ ulearnProject/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django import forms
 from ulearnProject import models
 
-admin.site.register(models.Vacancy)
+
+class VacancyForm(forms.ModelForm):
+    skills = forms.ModelMultipleChoiceField(
+        queryset=models.Skill.objects.all(),
+        widget=admin.widgets.FilteredSelectMultiple("Skills", is_stacked=False),
+        required=False,
+        label=''
+    )
+
+    class Meta:
+        model = models.Vacancy
+        fields = '__all__'
+
+class VacancyAdmin(admin.ModelAdmin):
+    form = VacancyForm
+
+admin.site.register(models.Vacancy, VacancyAdmin)
 admin.site.register(models.Skill)
-admin.site.register(models.VacancySkill)
--- ulearnProject/models.py
+++ ulearnProject/models.py
@@ -1,33 +1,27 @@
-from django.core.exceptions import ValidationError
 from django.db import models
-import hashlib
-
-
-class Vacancy(models.Model):
-    name = models.CharField(max_length=100)
-    salary = models.DecimalField(max_digits=10, decimal_places=0)
-    area_name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = 'vacancy'
-
-    @property
-    def skills(self):
-        return [item.skill.name for item in self.vacancyskill_set.all()]
 
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-        db_table = 'skill'
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
 
+    def __str__(self):
+        return self.name
+
+class Vacancy(models.Model):
+    name = models.CharField('Название вакансии', max_length=100)
+    salary = models.DecimalField('Зарплата', max_digits=10, decimal_places=0)
+    area_name = models.CharField('Город', max_length=100)
+    created_at = models.DateTimeField('Дата публикации', auto_now_add=True)
+    skills = models.ManyToManyField("Skill", through="VacancySkill")
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
 
 class VacancySkill(models.Model):
     vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE)
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'vacancy_skill'
-        unique_together = ('vacancy', 'skill')
