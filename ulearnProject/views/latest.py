0e001d54bbc5f68e7183edce97597c52947f7309
Kexogg <kexogg@gmail.com>
2024-01-18 23:11:29 +0500

    fix order

--- ulearnProject/views/latest.py
+++ ulearnProject/views/latest.py
@@ -4,6 +4,6 @@ from ulearnProject.models import Vacancy
 
 
 def latest(request):
-    vacancies = Vacancy.objects.order_by('created_at')[:5].all()
+    vacancies = Vacancy.objects.order_by('-created_at')[:5].all()
     print(vacancies)
     return render(request, 'latest.html', {"vacancies": vacancies})
