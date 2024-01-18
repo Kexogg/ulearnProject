24b1ed5bb555e380e14b952a387c4fd2fcee145d
Kexogg <kexogg@gmail.com>
2024-01-18 23:04:16 +0500

    last 5 vacancies

--- ulearnProject/views/latest.py
+++ ulearnProject/views/latest.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from ulearnProject.models import Vacancy
+
 
 def latest(request):
-    return render(request, 'latest.html', {})
+    vacancies = Vacancy.objects.order_by('created_at')[:5].all()
+    print(vacancies)
+    return render(request, 'latest.html', {"vacancies": vacancies})
